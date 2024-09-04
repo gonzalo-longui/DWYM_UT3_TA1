992f56e0276e74deb91c577ca41be5c65da9b9c1
longuigon <longuigon+git@gmail.com>
2024-09-04 09:55:56 -0300

    GET, POST & PUT hechos

--- script.js
+++ script.js
@@ -26,9 +26,110 @@ document.addEventListener('DOMContentLoaded', () => {
     const eliminarConfirmarModal = document.getElementById('confirmar-eliminar');
     const siEliminar = document.getElementById('si-eliminar');
     const noEliminar = document.getElementById('no-eliminar');
-
-    loadTasks();
     let currentTaskDiv = null;
+
+    function clearAllColumns() {
+        columns.forEach(column => {
+            const taskListContent = column.querySelector('.task-list-content');
+            if (taskListContent) {
+                taskListContent.innerHTML = '';
+            }
+        });
+    }
+
+    async function getTasksFromServer() {
+        let info;
+        try {
+            const response = await fetch(`http://localhost:3000/api/tasks`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const data = await response.json();
+            info = data;
+            console.log(info);
+            return info;
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    async function submitTaskToServer(task) {
+        try {
+            await fetch(`http://localhost:3000/api/tasks`, {
+                method: 'POST',
+                body: JSON.stringify(task),
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+            await showTasksInColumns();
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    async function editTaskInServer(task, taskDiv) {
+        try {
+            await fetch(`http://localhost:3000/api/tasks/${taskDiv.getAttribute('data-id')}`, {
+                method: 'PUT',
+                body: JSON.stringify(task),
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+            await showTasksInColumns();
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    async function showTasksInColumns() {
+        try {
+            clearAllColumns();
+            let tasks = await getTasksFromServer();
+            tasks.forEach(task => {
+                const taskDiv = document.createElement('div');
+                taskDiv.className = 'box';
+                taskDiv.setAttribute('data-id', task.id);
+                taskDiv.setAttribute('data-title', task.title);
+                taskDiv.setAttribute('data-description', task.description);
+                taskDiv.setAttribute('data-assigned', task.assignedTo);
+                taskDiv.setAttribute('data-priority', task.priority == 'High' 
+                    ? 'Alta' 
+                    : task.priority == 'Medium' 
+                    ? 'Media' 
+                    : 'Baja');
+                taskDiv.setAttribute('data-status', task.status.toLowerCase().replace(' ', '-'));
+                taskDiv.setAttribute('data-deadline', task.endDate);
+                taskDiv.setAttribute('draggable', 'true');
+                taskDiv.innerHTML = `<strong>${task.title}</strong><p>${task.description}</p>`;
+    
+                const column = document.getElementById(task.status.toLowerCase().replace(' ', '-'));
+                if (column) {
+                    column.querySelector('.task-list-content').appendChild(taskDiv);
+    
+                    taskDiv.addEventListener('dragstart', () => {
+                        taskDiv.classList.add('is-dragging');
+                    });
+    
+                    taskDiv.addEventListener('dragend', () => {
+                        taskDiv.classList.remove('is-dragging');
+                        saveTasks();
+                    });
+    
+                    taskDiv.addEventListener('click', () => openEditModal(taskDiv));
+                }
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }        
+    
+    showTasksInColumns();
+
+    // loadTasks();
 
     newTaskButton.addEventListener('click', () => {
         clearForm();
@@ -57,7 +158,7 @@ document.addEventListener('DOMContentLoaded', () => {
         eliminarConfirmarModal.classList.remove('is-active');
     }
 
-    taskForm.addEventListener('submit', (event) => {
+    taskForm.addEventListener('submit', async (event) => {
         event.preventDefault();
 
         const taskDiv = document.createElement('div');
@@ -71,8 +172,9 @@ document.addEventListener('DOMContentLoaded', () => {
         taskDiv.setAttribute('draggable', 'true');
         taskDiv.innerHTML = `<strong>${taskTitle.value}</strong><p>${taskDescription.value}</p>`;
 
-        const column = document.getElementById(taskStatus.value);
-        column.querySelector('.task-list-content').appendChild(taskDiv);
+        let taskJson = jsonifyTask(taskDiv);
+        console.log(taskJson);
+        await submitTaskToServer(taskJson);
 
         taskDiv.addEventListener('dragstart', () => {
             taskDiv.classList.add('is-dragging');
@@ -107,7 +209,8 @@ document.addEventListener('DOMContentLoaded', () => {
             if (draggingTask) {
                 const newStatus = column.id;
                 draggingTask.setAttribute('data-status', newStatus);
-                saveTasks(); 
+                let taskJson = jsonifyTask(draggingTask);
+                editTaskInServer(taskJson, draggingTask);
             }
         });
     });
@@ -136,6 +239,25 @@ document.addEventListener('DOMContentLoaded', () => {
         editTaskModal.classList.add('is-active');
     }
 
+    function jsonifyTask(taskDiv) {
+        let newTask = {};
+        newTask['assignedTo'] = taskDiv.getAttribute('data-assigned');
+        newTask['comments'] = [];
+        newTask['description'] = taskDiv.getAttribute('data-description');
+        newTask['endDate'] = taskDiv.getAttribute('data-deadline');
+        let priority = taskDiv.getAttribute('data-priority') == 'Alta' 
+            ? 'High' 
+            : taskDiv.getAttribute('data-priority') == 'Media' 
+            ? 'Medium' 
+            : 'Low';
+        newTask['priority'] = priority;
+        newTask['startDate'] = null;
+        newTask['status'] = taskDiv.getAttribute('data-status').replace('-', ' ').split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+        newTask['title'] = taskDiv.getAttribute('data-title');
+
+        return newTask
+    }
+
     editTaskForm.addEventListener('submit', (event) => {
         event.preventDefault();
 
@@ -146,6 +268,10 @@ document.addEventListener('DOMContentLoaded', () => {
         currentTaskDiv.setAttribute('data-status', editTaskStatus.value);
         currentTaskDiv.setAttribute('data-deadline', editTaskDeadline.value);
         currentTaskDiv.innerHTML = `<strong>${editTaskTitle.value}</strong><p>${editTaskDescription.value}</p>`;
+
+        let taskJson = jsonifyTask(currentTaskDiv);
+        editTaskInServer(taskJson, currentTaskDiv);
+
 
         const column = document.getElementById(editTaskStatus.value);
         if (column) {
@@ -163,6 +289,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     siEliminar.addEventListener('click', () => {
         currentTaskDiv.remove();
+        saveTasks();
         closeConfirmModal();
     })
 
@@ -196,37 +323,5 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
         localStorage.setItem('tasks', JSON.stringify(tasks));
-    }
-
-    function loadTasks() {
-        const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-        tasks.forEach(task => {
-            const taskDiv = document.createElement('div');
-            taskDiv.className = 'box';
-            taskDiv.setAttribute('data-title', task.title);
-            taskDiv.setAttribute('data-description', task.description);
-            taskDiv.setAttribute('data-assigned', task.assigned);
-            taskDiv.setAttribute('data-priority', task.priority);
-            taskDiv.setAttribute('data-status', task.status);
-            taskDiv.setAttribute('data-deadline', task.deadline);
-            taskDiv.setAttribute('draggable', 'true');
-            taskDiv.innerHTML = `<strong>${task.title}</strong><p>${task.description}</p>`;
-
-            const column = document.getElementById(task.status);
-            if (column) {
-                column.querySelector('.task-list-content').appendChild(taskDiv);
-
-                taskDiv.addEventListener('dragstart', () => {
-                    taskDiv.classList.add('is-dragging');
-                });
-
-                taskDiv.addEventListener('dragend', () => {
-                    taskDiv.classList.remove('is-dragging');
-                    saveTasks();
-                });
-
-                taskDiv.addEventListener('click', () => openEditModal(taskDiv));
-            }
-        });
     }
 });
